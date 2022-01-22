ecf33e6eca35ee0f1989df163f6a2dffb60f2072
Kurtis Doute <kcd257@yahoo.com>
2022-01-21 19:06:36 -0800

    Updated employee.test to pass tests and gather info for employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,33 @@
+const Employee = require('../lib/employee');
 
+it('creates an employee object', () => {
+    const employee = new Employee('Kurtis', 202, 'kcd257@yahoo.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+ 
+it('gets employee name', () => {
+    const employee = new Employee('Kurtis', 202, 'kcd257@yahoo.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+it('gets employee ID', () => {
+    const employee = new Employee('Kurtis', 202, 'kcd257@yahoo.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+it('gets employee email', () => {
+    const employee = new Employee('Kurtis', 202, 'kcd257@yahoo.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+it('gets role of employee', () => {
+    const employee = new Employee('Kurtis', 202, 'kcd257@yahoo.com');
+
+    expect(employee.getRole()).toEqual("Employee");
+}); 
